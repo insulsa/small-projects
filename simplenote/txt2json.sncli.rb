39864cd26cee890956001c461d5a681d60c47885
曹文明 <bunmei@mogami.futamiaki.info>
2016-05-12 13:18:39 +0900

    about to merge, this is end 1

--- simplenote/txt2json.sncli.rb
+++ simplenote/txt2json.sncli.rb
@@ -1,16 +1,30 @@
+def ignore(f)
+ #print f+"\0"
+end
+def uconv(s)
+ p=IO.popen({},["uchardet"],mode="w+")
+ p.write s
+ p.close_write
+ charset=p.read.chomp
+ return nil if charset == "ascii/unknown"
+ begin
+  charset="utf8" if charset == "UTF-8"
+  return s.encode("utf8",charset)
+ rescue Encoding::InvalidByteSequenceError, Encoding::InvalidByteSequenceError => e
+  STDERR.puts e
+  return nil
+ end
+end
+
 Dir.chdir "/home/bunmei/zombie-1/sncli"
 #set-search-path "."
 STDIN.read.split("\0").each { |f| 
- charset=`uchardet #{f}`.chomp
- if charset != "ascii/unknown"
- `iconv --from-code #{charset} #{f}|./sncli --nosync -t #{f} create -`
+ s=File.open(f).read
+ if f=uconv(f) and s=uconv(s)
+  IO.popen({},[["./sncli","argv0"],"--nosync","create","-"],mode="w").write f+"\n"+s
  else
-  puts f
-  next
+  ignore f
  end
- #s=File.open(f).read
- #s.encode!("utf8",charset)
- #IO.popen({},[["./sncli","argv0"],"--nosync","create","-"],mode="w").write 
 }
 # TODO
 # using the backtick boil the null delimeted filenames down
